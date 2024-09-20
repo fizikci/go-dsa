0027e6cccca4c34fcf9783f9d723f2fa354bd9e0
Bulent Keskin <bulent@vipdrv.com>
2024-09-20 23:46:36 +0200

    arrays.Uncompress

---
+++ arrays/uncompress.go
@@ -0,0 +1,29 @@
+package arrays
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+/*
+Write a function, Uncompress, that takes in a string as an argument. The input string will be formatted into
+multiple groups according to the following rules:
+*/
+
+func Uncompress(s string) string {
+	var result string
+	var lastNum string
+
+	for _, c := range s {
+		if unicode.IsDigit(c) {
+			lastNum += string(c)
+		} else {
+			num, _ := strconv.Atoi(lastNum)
+			result += strings.Repeat(string(c), num)
+			lastNum = ""
+		}
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -12,9 +12,9 @@ func main() {
 
 	//fmt.Println(string(arrays.MostFrequentChar("eleventennine")))
 
-	fmt.Println(arrays.PairSum([]int{3, 2, 5, 4, 1}, 8))
+	//fmt.Println(arrays.PairSum([]int{3, 2, 5, 4, 1}, 8))
 
-	//fmt.Println(arrays.Uncompress("3n12e2z"))
+	fmt.Println(arrays.Uncompress("3n12e2z"))
 
 	//fmt.Println(arrays.Compress("nnneeeeeeeeeeeezz"))
 }
