fe45ccf91d07733a9442f97d1c371b657132ac21
Bulent Keskin <bulent@vipdrv.com>
2024-09-20 23:44:40 +0200

    arrays.Anagrams

---
+++ arrays/anagrams.go
@@ -0,0 +1,28 @@
+package arrays
+
+/*
+Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean
+indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters,
+but in any order.
+*/
+
+func Anagrams(s1, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	counts := make(map[rune]int)
+
+	for _, r := range s1 {
+		counts[r]++
+	}
+
+	for _, r := range s2 {
+		counts[r]--
+		if counts[r] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"bulentkeskin.com/dsa/arrays"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	fmt.Println(arrays.Anagrams("hello", "ollah"))
+
+	//fmt.Println(string(arrays.MostFrequentChar("eleventennine")))
+
+	//fmt.Println(arrays.PairSum([]int{3, 2, 5, 4, 1}, 8))
+
+	//fmt.Println(arrays.Uncompress("3n12e2z"))
+
+	//fmt.Println(arrays.Compress("nnneeeeeeeeeeeezz"))
 }
